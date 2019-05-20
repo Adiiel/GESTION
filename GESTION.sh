86b3236641e28eb06afaa81ab60f60cef361bf0e
Adiiel <adielpreciado@upeu.edu.pe>
2019-05-20 16:10:01 +0000

    mi primer commit

--- GESTION.sh
+++ GESTION.sh
@@ -1,5 +1,3 @@
-#!/bin/sh
-#d=$(date +%Y-%m-%d)
 git add -A
-cd /home/adiel29/GESTION && git commit -m "'commit''date'"
-cd /home/adiel29/GESTION && git push -u origin master
+git commit -am 'mi primer commit'
+git push
